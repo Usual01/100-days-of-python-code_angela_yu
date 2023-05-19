a62379976ca847c762f57608e2d4b86e28cbe000
michael <mikedami001@gmail.com>
2023-05-19 09:57:47 +0100

    squared

---
+++ Advanced_Beginner/day26/squared.py
@@ -0,0 +1,7 @@
+"""
+write a new  list comprehension to create a new list squared_numbers 
+that contains the squared of the numbers
+"""
+
+squared_numbers = [s_num * s_num for s_num in range(1, 10)]
+print(squared_numbers)
