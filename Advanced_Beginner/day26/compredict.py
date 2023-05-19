4e2511cf21d137088792c91b42848cd0aadeb744
michael <mikedami001@gmail.com>
2023-05-19 13:55:36 +0100

    ungrateful

--- Advanced_Beginner/day26/compredict.py
+++ Advanced_Beginner/day26/compredict.py
@@ -1,4 +1,6 @@
 import random
 names = ["dami", "tobi", "moyo", "kemi", "afe", "sina", "miracle"]
 student_sscores = {student:random.randint(45, 100) for student in names}
+passed = {student:score for (student,score) in student_sscores.items() if score >= 60}
 print (student_sscores)
+print(passed)
