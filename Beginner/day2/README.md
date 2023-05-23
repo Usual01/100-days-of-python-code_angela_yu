de6a8e99658b2a422035423c07d22b3f0a331a95
michael <mikedami001@gmail.com>
2023-05-23 13:53:15 +0100

    day2

--- Beginner/day2/README.md
+++ Beginner/day2/README.md
@@ -2,6 +2,6 @@ created a tip calculator
 we learnt about datatypes
 type conversion
 conditionals
-and maths fumSctions
+and maths functions
 The tip calculator gets the total amount and splits it divides it by the total no of people
 gives a percentage of the total amount as tip returns the divided splitted bill
--- Beginner/day2/tip_calculator.py
+++ Beginner/day2/tip_calculator.py
@@ -5,7 +5,8 @@ write a tip calculator
 print("Welcome to the tip calculator\n") 
 bill  = float(input("what was the total bill\n"))
 percent = int(input("What percentage tip would you give? 10, 12 or 15?\n"))
-nump = int(input("How many peaople are to split this bill"))
+nump = int(input("How many people are to split this bill\n"))
 amount = float( bill / nump)
+format_amount = "{:.2f}".format(amount)
 tip = (percent / 100) * bill
-print(f"Each person should pay ${round(amount, 2)} and the tip is ${tip}")
+print(f"The total bill was {bill} Each person should pay ${format_amount} and the tip is ${tip}")
