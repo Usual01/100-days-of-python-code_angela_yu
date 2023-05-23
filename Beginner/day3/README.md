edfffd21ce5eccfa84ce8e049191b13c46311797
michael <mikedami001@gmail.com>
2023-05-23 16:26:28 +0100

    day3

---
+++ Beginner/day3/README.md
@@ -0,0 +1,4 @@
+we write a treasure map program that moves based on conditions
+we are taught
+nested conditional statements
+operators
---
+++ Beginner/day3/leep.py
@@ -0,0 +1,29 @@
+leap = int(input('Enter year'))
+
+
+if leap % 4 == 0 and leap % 100 != 0 or leap % 400 == 0:
+    print ("leap year")
+else:
+    print("not a leap year")
+
+    """
+ChatGPT is an advanced language model developed by OpenAI.
+It is based on the GPT-3.5 architecture, which stands for "Generative Pre-trained Transformer 3.5."
+GPT-3.5 is a deep learning model that utilizes a transformer neural network to process and generate human-like
+text responses.
+
+ChatGPT is designed to engage in natural language conversations with users,
+providing detailed and coherent responses. It has been trained on a massive amount of text data from the internet, 
+enabling it to understand and generate text across various topics and contexts.
+The model's training allows it to generate relevant and contextually appropriate responses, 
+making it useful for a wide range of conversational tasks.
+
+However, it's important to note that while ChatGPT can generate impressive responses,
+ it is still an AI model and has limitations. 
+ It may occasionally produce incorrect or nonsensical answers and 
+ can be sensitive to input phrasing or biased information present in its training data.
+It's always a good idea to verify information from multiple reliable sources when using AI-generated responses.
+
+"""
+
+
