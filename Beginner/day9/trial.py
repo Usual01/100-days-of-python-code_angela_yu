c046fc7172ea2ae9468e3dc5d05dd60e3115a545
michael <mikedami001@gmail.com>
2023-05-24 11:14:53 +0100

    day9

---
+++ Beginner/day9/trial.py
@@ -0,0 +1,15 @@
+travel_log = [
+    {
+    "country": "france",
+    "visits": 12,
+    "cities": ["paris", "lille", "Dijon"]
+    }, 
+    {
+    "country": "Germany",
+    "visits": 5,
+    "cities": ["Berlin", "Hamburg", "Sttugart"]
+    }
+]
+travel_log.append(["Russia", 2, ["Moscow", "saint-petersburg"]])
+travel_log.append([1])
+print(travel_log)
