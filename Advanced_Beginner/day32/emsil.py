f7693a220f98adb26e0fb468dd495bbe28474bbb
michael <mikedami001@gmail.com>
2023-06-03 13:38:58 +0100

    day32

---
+++ Advanced_Beginner/day32/emsil.py
@@ -0,0 +1,12 @@
+import smtplib
+
+my_email = ""
+password = ""
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user= my_email, password= password)
+    connection.sendmail(from_addr= my_email, 
+                        to_addrs="",
+                          msg= "Subject:hello\n\n long time")
+    connection.close()
