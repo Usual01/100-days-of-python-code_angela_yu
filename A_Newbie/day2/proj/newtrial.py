52ca6f2c219da97e0477a9fe87b28721e41358cf
michael <mikedami001@gmail.com>
2023-06-04 16:20:55 +0100

    day1

---
+++ A_Newbie/day2/proj/newtrial.py
@@ -0,0 +1,7 @@
+print("Welcome to the tip calculator\n")
+t_bill = float(input("What was the total bill $"))
+p_tip = int(input("What percentage tip would you like to give? 10, 12, 15\t"))
+people = int(input("how many people are spliting this bill?\t"))
+tip = p_tip / 100 * t_bill
+bill = (t_bill + tip) / people 
+print(f"Each person should pay ${bill}")
