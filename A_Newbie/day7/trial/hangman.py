609ecfdbce16236997b0a22bed96cf185b397620
michael <mikedami001@gmail.com>
2023-06-06 15:11:53 +0100

    day7

--- A_Newbie/day7/trial/hangman.py
+++ A_Newbie/day7/trial/hangman.py
@@ -13,7 +13,6 @@ user_word = input("what is the word\n").lower()
 for a in range(len(choice)):
     letter = choice[a]
     if letter == user_word:
-        #display[a] = letter
-        #display[a].replace(letter)
-        display.replace(display[a], letter)
+        display[a] = letter
+        
 print(display)
