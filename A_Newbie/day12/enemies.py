39d105c91c042d7d435cb625975d0f708a50587e
michael <mikedami001@gmail.com>
2023-06-08 06:55:27 +0100

    love

--- A_Newbie/day12/enemies.py
+++ A_Newbie/day12/enemies.py
@@ -1,8 +1,12 @@
 enemies = 1
 
+print(f"enemies before: {enemies}")
 def increase_enemies():
     print(f"enemies inside : {enemies}")
     return enemies + 1
+print(f"enemies after block: {enemies}")
 
 enemies = increase_enemies()
+print(f"enemies after assignnment: {enemies}")
+
 print(f"enemies outside : {enemies}")
