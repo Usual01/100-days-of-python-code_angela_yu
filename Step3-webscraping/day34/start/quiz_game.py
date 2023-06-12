11bfcc1e8fb7570431a36aed10ded5ddebb80513
michael <mikedami001@gmail.com>
2023-06-12 22:09:52 +0100

    love

--- Step3-webscraping/day34/start/quiz_game.py
+++ Step3-webscraping/day34/start/quiz_game.py
@@ -1,6 +1,7 @@
 from quizzer import question_data
 from question_model import Question
 from quiz_brain import QuizBrain
+from ui import QuizInterface
 
 question_bank = []
 for question in question_data:
@@ -10,5 +11,10 @@ for question in question_data:
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
-while quiz.still_has_questions():
-    quiz.next_question()
+quiz_ui = QuizInterface(quiz)
+
+# while quiz.still_has_questions()
+#     quiz.next_question()
+
+print("you've completed the quiz")
+print("your final score is {quiz.score} / {quiz.question_number}")
